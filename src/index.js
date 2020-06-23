75a54d65ce6648d174faba57290db29f0be1ae96
senadakadric <skadric@fordham.edu>
2020-06-23 14:59:27 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const taskDescription = event.target.description.value;
+
+    // do we even need to create an object??
+    // const taskObj = {
+    //   description: taskDescription,
+    // };
+
+    const addTask = document.createElement("li");
+    addTask.innerHTML = `${taskDescription}`;
+
+    const currentThing = document.getElementById("tasks");
+    currentThing.appendChild(addTask);
+  });
 });
